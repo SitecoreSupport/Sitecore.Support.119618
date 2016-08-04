0969ab51068567909351a8cbf4d668f8bb18a7a4
AlenPelin <AlenPelin@gmail.com>
2016-08-04 12:33:34 +1000

    Implemented.

---
+++ src/Sitecore.Support.119618/Sharepoint/Pipelines/ProcessIntegrationItem/UpdateName.cs
@@ -0,0 +1,40 @@
+﻿namespace Sitecore.Support.Sharepoint.Pipelines.ProcessIntegrationItem
+{
+  using System;
+  using Sitecore.Data.Items;
+  using Sitecore.Diagnostics;
+  using Sitecore.Sharepoint.Pipelines.ProcessIntegrationItem;
+
+  [UsedImplicitly]
+  public class UpdateName
+  {
+    [UsedImplicitly]
+    public UpdateName()
+    {
+    }
+
+    [UsedImplicitly]
+    public virtual void Process(ProcessIntegrationItemArgs args)
+    {
+      Assert.ArgumentNotNull(args, nameof(args));
+
+      var targetIntegrationItem = args.IntegrationItem;
+      var sourceSharepointItem = args.SourceSharepointItem;
+
+      Assert.IsNotNull(targetIntegrationItem, nameof(targetIntegrationItem));
+      Assert.IsNotNull(sourceSharepointItem, nameof(sourceSharepointItem));
+
+      var sourceName = sourceSharepointItem.Title;
+      var targetName = targetIntegrationItem.Name;
+      if (targetName.Equals(sourceName, StringComparison.Ordinal))
+      {
+        return;
+      }
+
+      using (new EditContext(targetIntegrationItem))
+      {
+        targetIntegrationItem.Name = sourceName;
+      }
+    }
+  }
+}
